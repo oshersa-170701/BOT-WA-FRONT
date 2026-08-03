72519208c24239bdf6dc7c7a6bbdb8089a11dbcb
oshersa-170701 <oshersa1771@gmail.com>
2026-08-02 18:34:46 -0600

    Cambio front 3

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,14 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    provideHttpClient()
+    provideHttpClient(),
+    provideHttpClient(withFetch()) // <--- REGISTRA ESTO AQUÍ
   ],
 });
